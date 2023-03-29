cde01061742497eb2bac7a7ea8fa47a1de0dc321
LuckYOneTop <124349839+LuckYOneTop@users.noreply.github.com>
2023-03-29 15:39:07 +0300

    add homework

---
+++ HW_3_5/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HW_3_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var asyncFileReader = new AsyncFileReader();
+            var result = asyncFileReader.FetchTextAsync().Result;
+        }
+    }
+}
